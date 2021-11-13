80c1fc426f6ba23442ff4163053550ee2cea4231
fatihshu <86612406+fatihshu@users.noreply.github.com>
2021-11-13 14:00:05 +0000

    first

--- src/test/java/com/HectorWare/runners/FailedTestRunner.java
+++ src/test/java/com/HectorWare/runners/FailedTestRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/failed-html-report"},
-        features = "@target/rerun.txt",
-        glue = "com/vytrack/step_definitions"
+        features = "@target/rerun.txt"
+
 )
 public class FailedTestRunner {
 }
