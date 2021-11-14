f6e669bc1f2f2d7ff2b8d8f34a222497d08e573f
fatihshu <86612406+fatihshu@users.noreply.github.com>
2021-11-14 02:13:36 +0000

    Created some WebElement

--- src/test/java/com/HectorWare/pages/ContacInfoPage.java
+++ src/test/java/com/HectorWare/pages/ContacInfoPage.java
@@ -1,4 +1,29 @@
 package com.HectorWare.pages;
 
-public class ContacInfoPage {
+import com.HectorWare.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ContacInfoPage extends BasePage {
+    public ContacInfoPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+    @FindBy(id="contact-fullname")
+    public WebElement fullName;
+
+    @FindBy(id = "contact-org")
+    public WebElement companyName;
+
+    @FindBy(id="contact-title")
+    public  WebElement title;
+
+    @FindBy(className = "icon action-item__menutoggle icon-picture-force-white")
+    public  WebElement FotoButton;
+
+@FindBy(xpath = "//*[@id=\"menu-ubzrn\"]/li[1]/button/span[2]")
+    public WebElement UplodFoto;
+
+@FindBy(xpath = "//*[@id=\"menu-ubzrn\"]/li[2]/button/span[2]")
+    public WebElement uplodFotoFromFile;
 }
