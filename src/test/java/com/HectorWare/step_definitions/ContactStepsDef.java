b4c720ae0863ad92983c1928d192020aa56b279f
fatihshu <86612406+fatihshu@users.noreply.github.com>
2021-11-20 15:23:50 +0000

    uptaded

--- src/test/java/com/HectorWare/step_definitions/ContactStepsDef.java
+++ src/test/java/com/HectorWare/step_definitions/ContactStepsDef.java
@@ -1,17 +1,21 @@
 package com.HectorWare.step_definitions;
 
+import com.HectorWare.pages.BasePage;
+import com.HectorWare.pages.ContacInfoPage;
 import com.HectorWare.pages.LoginPage;
+import com.HectorWare.utilities.ConfigurationReader;
+import com.HectorWare.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
-public class ContactStepsDef {
+public class ContactStepsDef extends BasePage {
 
     @Given("the user logs in with valid credentials")
     public void the_user_logs_in_with_valid_credentials() {
-        LoginPage loginPage=new LoginPage();
-        loginPage.login();
-
+        String url= ConfigurationReader.get("url");
+        Driver.get().get(url);
 
     }
 
@@ -22,6 +26,7 @@ public class ContactStepsDef {
 
     @Then("the New Contact page should be displayed")
     public void the_New_Contact_page_should_be_displayed() {
+
 
     }
 
